26146c8d77d938277c831d0e2919476e916fc160
Larry LeFever <larrydlefever@gmail.com>
2017-05-13 16:27:46 -0400

    changed whitespace

--- src/main/java/net/lefever/speechenabled/RESTClient.java
+++ src/main/java/net/lefever/speechenabled/RESTClient.java
@@ -14,145 +14,145 @@ import net.lefever.speechenabled.dto.Message;
 
 public class RESTClient
 {
-	public static enum EndPoint
-	{
-		TEST("/test"),
-		TEST_GET("/testGet");
+    public static enum EndPoint
+    {
+        TEST("/test"),
+        TEST_GET("/testGet");
 
-		String url;
+        String url;
+ 
+        EndPoint(String url)
+        {
+            this.url = url;
+        }
+    }
 
-		EndPoint(String url)
-		{
-			this.url = url;
-		}
-	}
+    private String baseUrl;
 
-	private String baseUrl;
+    public RESTClient(String url)
+    {
+        this.baseUrl = url;
+    }
 
-	public RESTClient(String url)
-	{
-		this.baseUrl = url;
-	}
+    public static void main(String[] args)
+    {
+        RESTClient client = new RESTClient("http://localhost:8080");
 
-	public static void main(String[] args)
-	{
-		RESTClient client = new RESTClient("http://localhost:8080");
+        Message inMsg = new Message();
+        inMsg.setStatus("foo");
 
-		Message inMsg = new Message();
-		inMsg.setStatus("foo");
+        Message message = client.doPost(EndPoint.TEST, inMsg, Message.class);
+        System.out.println("status:" + message.getStatus());
+        
+        message = client.doGet(EndPoint.TEST_GET, Message.class);
+        System.out.println("status:" + message.getStatus());
+    }
 
-		Message message = client.doPost(EndPoint.TEST, inMsg, Message.class);
-		System.out.println("status:" + message.getStatus());
-		
-		message = client.doGet(EndPoint.TEST_GET, Message.class);
-		System.out.println("status:" + message.getStatus());
-	}
+    public <T> T doPost(EndPoint endPoint, Message inMsg, Class<T> outMsgtype)
+    {
+        HttpURLConnection conn = null;
+        JsonWriter writer = null;
+        JsonReader reader = null;
 
-	public <T> T doPost(EndPoint endPoint, Message inMsg, Class<T> outMsgtype)
-	{
-		HttpURLConnection conn = null;
-		JsonWriter writer = null;
-		JsonReader reader = null;
+        try
+        {
+            URL url = new URL(getBaseUrl() + endPoint.url);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
 
-		try
-		{
-			URL url = new URL(getBaseUrl() + endPoint.url);
-			conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Type", "application/json");
+            Gson gson = new Gson();
+            OutputStream out = conn.getOutputStream();
+            writer = new JsonWriter(new OutputStreamWriter(out, "UTF-8"));
+            gson.toJson(inMsg, inMsg.getClass(), writer);
 
-			Gson gson = new Gson();
-			OutputStream out = conn.getOutputStream();
-			writer = new JsonWriter(new OutputStreamWriter(out, "UTF-8"));
-			gson.toJson(inMsg, inMsg.getClass(), writer);
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK)
+            {
+                throw new RuntimeException("Failed: HTTP error code: " + conn.getResponseCode());
+            }
 
-			if (conn.getResponseCode() != HttpURLConnection.HTTP_OK)
-			{
-				throw new RuntimeException("Failed: HTTP error code: " + conn.getResponseCode());
-			}
+            reader = new JsonReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            T message = gson.fromJson(reader, outMsgtype);
 
-			reader = new JsonReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-			T message = gson.fromJson(reader, outMsgtype);
+            return message;
+        } 
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        } 
+        finally
+        {
+            if (null != conn)
+            {
+                conn.disconnect();
+            }
+            if (null != reader)
+            {
+                try
+                {
+                    reader.close();
+                } 
+                catch (IOException e)
+                {
+                }
+            }
+            if (null != writer)
+            {
+                try
+                {
+                    writer.close();
+                } 
+                catch (IOException e)
+                {
+                }
+            }
+        }
 
-			return message;
-		} 
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		} 
-		finally
-		{
-			if (null != conn)
-			{
-				conn.disconnect();
-			}
-			if (null != reader)
-			{
-				try
-				{
-					reader.close();
-				} 
-				catch (IOException e)
-				{
-				}
-			}
-			if (null != writer)
-			{
-				try
-				{
-					writer.close();
-				} 
-				catch (IOException e)
-				{
-				}
-			}
-		}
+        return null;
+    }
 
-		return null;
-	}
+    public <T> T doGet(EndPoint endPoint, Class<T> outMsgtype)
+    {
+        HttpURLConnection conn = null;
+        JsonReader reader = null;
 
-	public <T> T doGet(EndPoint endPoint, Class<T> outMsgtype)
-	{
-		HttpURLConnection conn = null;
-		JsonReader reader = null;
+        try
+        {
 
-		try
-		{
+            URL url = new URL(getBaseUrl() + endPoint.url);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
 
-			URL url = new URL(getBaseUrl() + endPoint.url);
-			conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-			conn.setRequestProperty("Accept", "application/json");
+            if (conn.getResponseCode() != 200)
+            {
+                throw new RuntimeException("Failed: HTTP error code: " + conn.getResponseCode());
+            }
 
-			if (conn.getResponseCode() != 200)
-			{
-				throw new RuntimeException("Failed: HTTP error code: " + conn.getResponseCode());
-			}
+            Gson gson = new Gson();
+            reader = new JsonReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            T message = gson.fromJson(reader, outMsgtype);
 
-			Gson gson = new Gson();
-			reader = new JsonReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-			T message = gson.fromJson(reader, outMsgtype);
+            return message;
+        } 
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        } 
+        finally
+        {
+            if (null != conn)
+            {
+                conn.disconnect();
+            }
+        }
 
-			return message;
-		} 
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		} 
-		finally
-		{
-			if (null != conn)
-			{
-				conn.disconnect();
-			}
-		}
+        return null;
+    }
 
-		return null;
-	}
-
-	protected String getBaseUrl()
-	{
-		return this.baseUrl;
-	}
+    protected String getBaseUrl()
+    {
+        return this.baseUrl;
+    }
 }
